496ba731e87fd734cd11f0775f88187ce1e9dcaf
shyamal <shyamalranpara7846@gmail.com>
2024-03-20 14:57:59 +0530

    project 1

---
+++ project 1/task 1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main(){
+    int a=97;
+    do{
+        printf("%c\n",a);
+        a+=4;   
+    }while (a<=122);
+
+}
---
+++ project 1/task 2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main(){
+    int l,j=0;
+
+    printf("enter any number:");
+    scanf("%d",&l);
+
+    for (int i=0; i <=10 ; i++)
+    {
+        
+      if (l==0)
+      {
+        break;
+      }
+      l/=10;
+      j++;
+       
+    }
+    
+     printf("print the total num:%d",j);
+
+}

14ed6a714c2960268259e757a0116070b11adb35
Shyamalranpara <162433756+Shyamalranpara@users.noreply.github.com>
2024-07-22 10:08:31 +0530

    Update task 5.c

--- n pr-4/task 5.c
+++ n pr-4/task 5.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-
-int main() {
-    int n = 5;
-    for(int i = 1; i <= n; i++) {
-        // Print leading spaces
-        for(int j = n; j > i; j--) {
-            printf("  ");
+main(){
+    for (int i = 1; i <= 5; i++){
+        for (int j = 5; j > i; j--)
+        {
+            printf(" ");
         }
-        // Print decreasing numbers
-        for(int k = i; k > 1; k--) {
-            printf("%d ", k);
-        }
-        // Print increasing numbers
-        for(int l = 7; l <= i; l++) {
-            printf("%d ", l);
+        
+        for (int j = i*2-1; j >= 1; j--){
+            printf("%d",j);
         }
         printf("\n");
     }
-    return 0;
 }

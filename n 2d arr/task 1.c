5ca8c8e2d5a9a3a2e182cfc62f4ed87f71e43fb7
Shyamalranpara <162433756+Shyamalranpara@users.noreply.github.com>
2024-07-26 10:29:53 +0530

    Update task 1.c

--- n 2d arr/task 1.c
+++ n 2d arr/task 1.c
@@ -1,4 +1,27 @@
-#include<stdio.h>
-int main(){
-    
+#include <stdio.h>
+main()
+{
+    int size;
+    float sum = 0.00;
+    printf("Enter size of 2d array : ");
+    scanf("%d", &size);
+
+    int arr[size][size];
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++){
+            printf("Enter number for arr[%d][%d] : ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++){
+            sum += arr[i][j];
+        }
+    }
+    printf("size of arr: %d\n", size*size);
+    printf("sum of arr : %0.2f\n", sum);
+    printf("average of arr : %0.2f\n", sum / size);
+    main();
 }

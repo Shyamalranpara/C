d21e2b3f7b59657ec1670d3dfb832f78aebfa3e8
Shyamalranpara <162433756+Shyamalranpara@users.noreply.github.com>
2024-07-28 21:43:31 +0800

    Update task 4.c

--- n 2d arr/task 4.c
+++ n 2d arr/task 4.c
@@ -26,32 +26,3 @@ main()
     }
     main();
 } 
-=======
-#include <stdio.h>
-main()
-{
-    int size;
-    printf("Enter size of 2d array : ");
-    scanf("%d", &size);
-    int arr[size][size];
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++){
-            printf("Enter number for arr[%d][%d] : ", i, j);
-            scanf("%d", &arr[i][j]);
-        }
-    }
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++){
-            if (i == 0 || i == size-1 || j == 0 || j == size-1){
-                printf("%d ", arr[i][j]);
-            }else{
-                printf("  ");
-            }
-        }
-        printf("\n");
-    }
-    main();
-} 
->>>>>>> 2ab6b2769d620eaccb3ba2fa82398cb7666d32f3

3bce71bf12eda47d28cfc1d8e0103498368fe9fc
Shyamalranpara <162433756+Shyamalranpara@users.noreply.github.com>
2024-08-21 11:09:44 +0530

    Add files via upload

---
+++ n practical/question 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int sum(){
+    int n,sum=0,size;
+    
+    printf("enter the element: ");
+    scanf("%d",&n);
+
+      int[size];
+
+    for (int i = 1; i <=size ; i++)
+    {
+    
+        sum+=n+size[i];
+        printf("%d\n",sum);
+    }
+    
+}
